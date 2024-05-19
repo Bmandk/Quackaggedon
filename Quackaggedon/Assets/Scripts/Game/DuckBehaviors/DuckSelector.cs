93f6464c8d5f04f35946b4a06d7532917ec5b0c8
miro.brodlova <miro.brodlova@unity3d.com>
2024-05-19 14:33:05 +0200

    Add material change on hover

--- Quackaggedon/Assets/Scripts/Game/DuckBehaviors/DuckSelector.cs
+++ Quackaggedon/Assets/Scripts/Game/DuckBehaviors/DuckSelector.cs
@@ -1,25 +1,50 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class DuckSelector : MonoBehaviour
 {
+    [SerializeField]
+    private Material _defaultMat;
+    
+    [SerializeField]
+    private Material _highlightMat;
+    
+    [SerializeField]
+    private Material _selectMat;
+    
+    [SerializeField]
+    private SpriteRenderer[] _duckSprites;
+
+    private bool isSelected;
     public void Select()
     {
-        
+        isSelected = true;
+        ChangeDuckMaterial(_selectMat);
     }
     
     public void Deselect()
     {
-        
+        isSelected = false;
+        ChangeDuckMaterial(_defaultMat);
     }
     
     public void Hover()
     {
-        
+        ChangeDuckMaterial(_highlightMat);
     }
     
     public void Unhover()
     {
-        
+        if (!isSelected)
+            ChangeDuckMaterial(_defaultMat);
+    }
+
+    private void ChangeDuckMaterial(Material newMaterial)
+    {
+        foreach (var sprite in _duckSprites)
+        {
+            sprite.material = newMaterial;
+        }   
     }
 }
