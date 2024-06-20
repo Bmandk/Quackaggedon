d17bf6c37659a4ebd75fcb4c8e1da29f421cb0e4
Mi <miro.brodlova@gmail.com>
2024-06-20 10:00:22 +0200

    Fix order in silhouttes, add tooltip on hut ducks

--- Quackaggedon/Assets/ToolTipDisplayDuckIfDiscovered.cs
+++ Quackaggedon/Assets/ToolTipDisplayDuckIfDiscovered.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ToolTipDisplayDuckIfDiscovered : MonoBehaviour
+public class ToolTipDisplayDuckIfDiscovered : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 {
     public DuckType duckType;
 
