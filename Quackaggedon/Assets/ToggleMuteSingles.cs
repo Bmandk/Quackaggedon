894fef2287d65ba333c33592c91e416092abaa67
Mi <miro.brodlova@gmail.com>
2024-06-20 23:48:38 +0200

    Add sound toggles into intro and outro too

---
+++ Quackaggedon/Assets/ToggleMuteSingles.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleMuteSingles : MonoBehaviour
+{
+    public Toggle muteToggle;
+
+    private void Awake()
+    {
+        muteToggle.isOn = false;
+        OnValueChanged();
+    }
+
+    private void RefreshVolume()
+    {
+        if (PlayerPrefs.HasKey("SoundMuted"))
+        {
+            muteToggle.isOn = PlayerPrefs.GetInt("SoundMuted") == 1 ? true : false;
+
+            if (!muteToggle.isOn)
+            {
+                if (PlayerPrefs.HasKey("Volume"))
+                {
+                    AudioListener.volume = PlayerPrefs.GetFloat("Volume");
+                }
+            }
+            else
+            {
+                AudioListener.volume = 0;
+            }
+        }
+    }
+
+    public void OnValueChanged()
+    {
+        PlayerPrefs.SetInt("SoundMuted", muteToggle.isOn ? 1 : 0);
+        RefreshVolume();
+
+        float setVolume = 0;
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            setVolume = PlayerPrefs.GetFloat("Volume");
+        }
+
+        if (!muteToggle.isOn && setVolume == 0)
+        {
+            AudioListener.volume = 0.6f;
+        }
+    }
+}
