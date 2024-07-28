db954f42544affd1a6e714ce265d1810de455686
Bmandk <jonathan_hertz2@live.dk>
2024-07-28 17:19:02 +0200

    Add WIN achievement

--- Quackaggedon/Assets/Scripts/EnableContOnFinishType.cs
+++ Quackaggedon/Assets/Scripts/EnableContOnFinishType.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Steamworks;
 using UnityEngine;
 
 public class EnableContOnFinishType : MonoBehaviour
@@ -7,7 +8,14 @@ public class EnableContOnFinishType : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string achievement = "WIN";
+        SteamUserStats.GetAchievement(achievement, out bool achieved);
+
+        if (!achieved)
+        {
+            SteamUserStats.SetAchievement(achievement);
+            SteamUserStats.StoreStats();
+        }
     }
 
     // Update is called once per frame
